f2c9197cecbd7331a52bd253cbdf7537586b7445
RudenkoOleksandr02 <your_email@example.com>
2025-03-17 12:33:01 +0200

    fix

--- app/(root)/not-found.tsx
+++ app/(root)/not-found.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import {InfoBlock} from "@/components/shared";
 
 export default function NotFound() {
--- components/shared/stories.tsx
+++ components/shared/stories.tsx
@@ -9,10 +9,10 @@ import {X} from "lucide-react";
 import ReactStories from 'react-insta-stories';
 
 interface Props {
-  className?: string;
+    className?: string;
 }
 
-export const Stories: React.FC<Props> = ({ className }) => {
+export const Stories: React.FC<Props> = ({className}) => {
     const [stories, setStories] = React.useState<IStory[]>([]);
     const [open, setOpen] = React.useState(false);
     const [selectedStory, setSelectedStory] = React.useState<IStory>();
@@ -40,31 +40,30 @@ export const Stories: React.FC<Props> = ({ className }) => {
         >
             {stories.length === 0 &&
                 [...Array(6)].map((_, index) => (
-                    <div key={index} className="w-[200px] h-[200px] bg-gray-200 rounded-md animate-pulse" />
+                    <div key={index} className="w-[200px] h-[200px] bg-gray-200 rounded-md animate-pulse"/>
                 ))
             }
             {stories.map((story) => (
-                <div className="flex-1">
-                <img
-                    key={story.id}
-                    onClick={() => onClickStory(story)}
-                    className="rounded-md cursor-pointer object-cover"
-                    src={story.previewImageUrl}
-                    alt={`story ` + story.id}
-                />
+                <div className="flex-1" key={story.id}>
+                    <img
+                        onClick={() => onClickStory(story)}
+                        className="rounded-md cursor-pointer object-cover"
+                        src={story.previewImageUrl}
+                        alt={`story ` + story.id}
+                    />
                 </div>
             ))}
 
             {open && (
                 <div className="fixed left-0 top-0 w-full h-full bg-black/80 flex items-center justify-center z-30">
-                    <div className="relative" style={{ width: 520 }}>
+                    <div className="relative" style={{width: 520}}>
                         <button className="absolute -right-10 -top-5 z-30" onClick={() => setOpen(false)}>
-                            <X className="absolute top-0 right-0 w-8 h-8 text-white/50" />
+                            <X className="absolute top-0 right-0 w-8 h-8 text-white/50"/>
                         </button>
 
                         <ReactStories
                             onAllStoriesEnd={() => setOpen(false)}
-                            stories={selectedStory?.items.map((item) => ({ url: item.sourceUrl })) || []}
+                            stories={selectedStory?.items.map((item) => ({url: item.sourceUrl})) || []}
                             defaultInterval={3000}
                             width={520}
                             height={800}
